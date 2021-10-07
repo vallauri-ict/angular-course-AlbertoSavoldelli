666cf623136b441ee0271f00efd1725e22a4f645
AlbertoSavoldelli <a.savoldelli.1375@vallauri.edu>
2021-10-07 11:16:01 +0200

    Lesson 1 - Step 4: ngClass directive applied to student component

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -6,8 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student.component.css']
 })
 export class StudentComponent implements OnInit {
+  studentList=[
+    {name:'Ettore Esposito',hobby:'Karate',gender:'M'},
+    {name:'Edoardo Ababei',hobby:'Calcio',gender:'M'},
+    {name:'Fabio Cavallero',hobby:'Calcio',gender:'M'},
+    {name:'Lorenzo Cravero',hobby:'Basket',gender:'M'},
+    {name:'Martina Velardi',hobby:'Karate',gender:'F'},
+    {name:'Alberto Savoldelli',hobby:'Basket',gender:'M'},
+    {name:'Paolo Racca',hobby:'Karate',gender:'M'},
+    {name:'Giada Valinotto',hobby:'Calcio',gender:'F'},
+    {name:'Bianca Teleman',hobby:'Basket',gender:'F'},
+    {name:'Gabriele Leone',hobby:'Basket',gender:'M'},
+    {name:'Luca Pelissero',hobby:'Calcio',gender:'M'},
+  ]
 
-  constructor() { }
+  student:any;
+  constructor() {
+    let num=Math.floor(Math.random() * this.studentList.length)
+    this.student=this.studentList[num];
+   }
 
   ngOnInit(): void {
   }
